85abaaf704e12ffc4781f2917528061649985814
Zdw414 <zdw10030@saintjoe.edu>
2015-02-18 14:48:41 -0500

    dding number 4 from the test

---
+++ ArrayofObjects.js
@@ -0,0 +1,18 @@
+	// ZachDickinson
+	// this code creates an array and does something with it
+var A = [];
+for (var i = 0; i < 5; i ++){
+A.push(i);
+}
+console.log(A);
+function print(i) {
+  
+  console.log(A[i]);
+}
+print(0);
+print(1);
+print(2);
+
+
+
+
